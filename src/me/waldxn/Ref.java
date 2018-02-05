f511178b87a9a2efcca312ac0dcfaa0fb047d0ff
Waldxn <chriswalden80@gmail.com>
2018-02-05 16:13:19 -0500

    Fixed for Indite Solutions

--- src/me/waldxn/Ref.java
+++ src/me/waldxn/Ref.java
@@ -2,16 +2,21 @@ package me.waldxn;
 
 public class Ref {
 
+    //General Bot Settings
     static String TOKEN = "";
     static String GUILD_ID = "";
     public static String PREFIX = "-";
+
+    //Information for the Info command
     public static String WEBSITE = "";
     public static String TWITTER = "";
     public static String EMAIL = "";
     public static String PORTFOLIO = "";
 
+    //Category IDs
     public static String SUPPORT_ID = "";
 
+    //Role IDs
     public static String CLIENT_ROLE = "";
 
     public static int getInteger(String s) {
